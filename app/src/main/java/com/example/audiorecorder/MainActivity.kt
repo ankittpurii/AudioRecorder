f1030273ae738148426607831a462d1d0f677002
ankittpurii <puriankit31@gmail.com>
2024-07-26 23:25:23 +0530

    Handled bottom three buttons functionality

--- app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore.Audio.Media
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
@@ -15,6 +16,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.thezone.audiorecorder.Timer
+import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -28,15 +30,23 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener{
     private var dirPath=""
     private var fileName=""
     private lateinit var  recordButton :ImageButton
+    private lateinit var  doneButton :ImageButton
+    private lateinit var  listButton :ImageButton
+    private lateinit var  deleteButton :ImageButton
     private lateinit var  timerText :TextView
     private lateinit var timer: Timer
 
     private var isRecording = false
     private var isPaused = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recordButton= findViewById<ImageButton>(R.id.recordButton)
+        doneButton= findViewById<ImageButton>(R.id.doneButton)
+        listButton= findViewById<ImageButton>(R.id.listButton)
+        deleteButton= findViewById<ImageButton>(R.id.deleteButton)
         timerText= findViewById<TextView>(R.id.timer)
         permissionGranted= ActivityCompat.checkSelfPermission(this, permissions[0])==PackageManager.PERMISSION_GRANTED
         if(!permissionGranted)
@@ -52,7 +62,18 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener{
                 else -> startRecording()
         }
         }
-
+        listButton.setOnClickListener{
+            Toast.makeText(this, "List", Toast.LENGTH_SHORT).show()
+        }
+        doneButton.setOnClickListener{
+            Toast.makeText(this, "Record saved", Toast.LENGTH_SHORT).show()
+        }
+        deleteButton.setOnClickListener{
+            stopRecorder()
+            File("$dirPath$fileName.mp3").delete()
+            Toast.makeText(this, "Record deleted", Toast.LENGTH_SHORT).show()
+        }
+        deleteButton.isClickable = false
     }
 
     private fun resumeRecording(){
@@ -70,6 +91,23 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener{
 
     private fun stopRecorder(){
             timer.stop()
+        recorder.apply {
+            stop()
+            release()
+        }
+
+        isPaused = false
+        isRecording = false
+
+        listButton.visibility = View.VISIBLE
+        doneButton.visibility = View.GONE
+
+        deleteButton.isClickable = false
+        deleteButton.setImageResource(R.drawable.ic_delete_disabled)
+
+        recordButton.setImageResource(R.drawable.ic_record)
+
+        timerText.text = "00:00.00"
     }
 
     override fun onRequestPermissionsResult(
@@ -116,6 +154,11 @@ class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener{
             recordButton.setImageResource(R.drawable.ic_pause)
             isRecording=true
             isPaused=false
+            deleteButton.isClickable = true
+            deleteButton.setImageResource(R.drawable.ic_delete)
+
+            listButton.visibility = View.GONE
+            doneButton.visibility = View.VISIBLE
         }
     }
 
